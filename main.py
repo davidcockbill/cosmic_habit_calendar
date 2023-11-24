040445f87d0395c426a5dfafff51f6280fcf3800
David Cockbill <david.cockbill@screamingfrog.co.uk>
2023-11-24 08:23:23 +0000

    Main Controller

--- main.py
+++ main.py
@@ -5,24 +5,30 @@ from context import Context
 from wifi import Wifi
 from habit_calendar import HabitCalendar
 
-context = Context()
-wifi = Wifi(context)
-habit_calendar = HabitCalendar(context)
 
-def loop():
-    duration = context.process_button()
-    if duration > 100:
-        if duration < 1000:
-            habit_calendar.button_pressed()
-        else:
-            print(f'Long push {duration}')
+class Controller:
+    def __init__(self):
+        self.context = Context()
+        self.habit_calendar = HabitCalendar(self.context)
 
-    habit_calendar.refresh_display()
+    def run(self):
+        Wifi(self.context).sync_time()
+        self.context.set_brightness(0.8);
+        while True:
+            self.loop()
+            time.sleep(0.01)
+
+    def loop(self):
+        duration = self.context.process_button()
+        if duration > 100:
+            if duration < 1000:
+                self.habit_calendar.button_pressed()
+            else:
+                print(f'Long push {duration}')
+
+        self.habit_calendar.refresh_display()
 
 
 if __name__ == "__main__":
-    wifi.sync_time()
-    context.set_brightness(0.8);
-    while True:
-        loop()
-        time.sleep(0.01)
+    Controller().run()
+
